e2c1e48c7cc95ad5f61d5d9c8fec123a55dc30bc
louiseboseremen@gmail.com <louiseboseremen@gmail.com>
2024-10-23 10:10:59 +0100

    Changes

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -13,9 +13,6 @@ const [defaultCountries, setDefaultCountries] = useState(
   )
 );
 
-// useState array to store countries after being filtered by region
-const [filteredCountries, setFilteredCountries] = useState([]);
-
 const [searchedCountry, setSearchedCountry] = useState([]);
 
 // Function to handle region click and filter countries
@@ -62,7 +59,7 @@ const lightMode = () => {
 
 const backButton = () => {
   document.getElementById("searched-country").style.display="none";
-  document.getElementById("default-countries").style.display="block";
+  document.getElementById("default-countries").style.display="flex";
   document.getElementById("filter-div").style.display="flex";
   document.getElementById("back-div").style.display="none";
 }
@@ -92,33 +89,33 @@ function closeRegions(){
           </div>
           <input id="country-input" onChange={(e) => filterbySearch(e.target.value)} className="country-input text-center rounded p-2 m-3" type="text" placeholder="Search for a country ..." />
 
-          <div id="filter-div" className="filter-div m-3 p-3 w-50 rounded flex-row">
+          <div id="filter-div" className="filter-div m-3 p-3 rounded flex-row">
             <p>Filter by region</p>
             <FontAwesomeIcon onClick={displayRegions} id="caret-up" className="caret-up" icon={faCaretUp} /> 
             <FontAwesomeIcon onClick={closeRegions} id="caret-down" className="caret-down" icon={faCaretDown} /> 
           </div>
 
-          <div id="back-div" className="back-div m-3 p-3 w-50 rounded">
+          <div id="back-div" className="back-div m-3 p-3 rounded">
           <FontAwesomeIcon onClick={backButton} className="mx-2 mt-1" icon={faBackward} />
             Back
           </div>
 
-          <div id="regions-div" className="regions-div mx-3 p-2 w-50 position-absolute rounded">
-            <p onClick={() => filterByRegion("Africa")}>Africa</p>
-            <p onClick={() => filterByRegion("Americas")}>Americas</p>
-            <p onClick={() => filterByRegion("Asia")}>Asia</p>
-            <p onClick={() => filterByRegion("Europe")}>Europe</p>
-            <p onClick={() => filterByRegion("Oceania")}>Oceania</p>
+          <div id="regions-div" className="regions-div mx-3 p-2 position-absolute rounded">
+            <p className="region-p" onClick={() => filterByRegion("Africa")}>Africa</p>
+            <p className="region-p" onClick={() => filterByRegion("Americas")}>Americas</p>
+            <p className="region-p" onClick={() => filterByRegion("Asia")}>Asia</p>
+            <p className="region-p" onClick={() => filterByRegion("Europe")}>Europe</p>
+            <p className="region-p" onClick={() => filterByRegion("Oceania")}>Oceania</p>
           </div>
 
-            <ul id="default-countries">
+            <ul className="default-countries" id="default-countries">
             {defaultCountries.map((country) => (
           <li
-            className="job-li rounded list-item list-group-item mt-5"
+            className="rounded list-item list-group-item mt-5"
             key={country.alpha3Code}
           >
             <img
-              className="logo-img"
+              className="logo-img1"
               style={{ width: "300px" }}
               src={country.flags.svg}
               alt={`{job.company} logo`}
@@ -131,32 +128,33 @@ function closeRegions(){
              ))}
             </ul>
 
-            <ul id="searched-country">
+            <ul className="searched country" id="searched-country">
             {searchedCountry.map((country) => (
           <li
-            className="job-li rounded list-item list-group-item mt-5"
+            className="country-li rounded list-item list-group-item mt-5"
             key={country.alpha3Code}
           >
             <img
-              className="logo-img"
-              style={{ width: "300px",
-                height: "250px" 
-              }}
+              className="logo-img2"
               src={country.flags.svg}
-              alt={`{job.company} logo`}
+              alt={`Country img`}
             />
-            <p className="mt-4 country-name h1">{country.name? country.name : "Unknown Name"}</p>
-            <p className="mt-3"><span className="fw-bold">Native Name:</span> {country.nativeName? country.nativeName : "Unknown Native Name"}</p>
+            <div className="country-info1">
+            <p className="country-name h1">{country.name? country.name : "Unknown Name"}</p>
+            <p className="native-name"><span className="fw-bold">Native Name:</span> {country.nativeName? country.nativeName : "Unknown Native Name"}</p>
             <p><span className="fw-bold">Population:</span> {country.population? country.population : "Unknown Population"}</p>
             <p><span className="fw-bold">Region:</span> {country.region? country.region : "Unknown Region"}</p>
             <p><span className="fw-bold">Sub Region:</span> {country.subregion? country.subregion : "Unknown Subregion"}</p>
             <p><span className="fw-bold">Capital:</span> {country.capital? country.capital: "Unknown Capital"}</p>
+            </div>
+            <div className="country-info2">
             <p className="mt-4"><span className="fw-bold">Top Level Domain:</span> { country.topLevelDomain? country.topLevelDomain : "Unknown Top Level Domain"}</p>
             <p><span className="fw-bold">Currencies:</span> {country.currencies?.[0]?.name || 'No currency available'}</p>
             <p><span className="fw-bold">Languages:</span> {country.languages?.length ? country.languages?.map(lang => lang.name).join(', ') : 'Unknown Language'}</p> {/* Displaying languages */}
-            <div>
-            <p><span className="fw-bold">Border Countries:</span> </p>
-            <div>{country.borders?.join(', ') || 'No Border country'}</div>
+            </div>
+            <div className="border-div d-flex flex-column flex-wrap">
+            <p><span className="fw-bold">Border Countries:</span></p>
+            {country.borders?.join(', ') || 'No Border country'}
             </div>
             </li>
              ))}
